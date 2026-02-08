dff506286457a9f2e4fb87606b725aca389ff6cf
Vishnu-GovindarajPG <141429989+Vishnu-GovindarajPG@users.noreply.github.com>
2026-02-08 16:49:32 +0530

    Fix CI linting and test dependencies

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,13 +1,20 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ["eslint:recommended", "plugin:react-hooks/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended"
+  ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: {
     ecmaVersion: "latest",
-    sourceType: "module"
+    sourceType: "module",
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname
   },
-  plugins: ["react-refresh"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react-refresh"],
   rules: {
     "react-refresh/only-export-components": ["warn", { allowConstantExport: true }]
   }
